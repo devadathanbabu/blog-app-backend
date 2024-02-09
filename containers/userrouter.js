be17c7ce96ceae93cfd692da18ad8b6c060d1b0f
devadathanbabu <devadathanbabu27@gmail.com>
2024-02-09 13:37:03 +0530

    sigin added

--- containers/userrouter.js
+++ containers/userrouter.js
@@ -13,18 +13,59 @@ const userModel=require("../models/usermodel")
  
     let {data}={"data":req.body}
     let password=data.password
-    hashPasswordGenerator(password).then(
-        (hashedPassword)=>{
-            console.log(hashedPassword)
-            data.password=hashedPassword
-            console.log(data)
-            let user=new userModel(data)
-            let result= user.save()
+    // hashPasswordGenerator(password).then(
+    //     (hashedPassword)=>{
+    //         console.log(hashedPassword)
+    //         data.password=hashedPassword
+    //         console.log(data)
+    //         let user=new userModel(data)
+    //         let result= user.save()
+    //         res.json({
+    //             status:"success"
+    //             })
+    //     }
+    // )    
+    const hashedPassword=await hashPasswordGenerator(password)
+    data.password=hashedPassword
+    let user=new userModel(data)
+            let result=await user.save()
             res.json({
-                status:"success"
-                })
-        }
-    )    
+                 status:"success"
+                 })
+    
+ })
+
+ router.post("/signin",async(req,res)=>{
+    let input=req.body
+    let eMail=req.body.eMail
+    let data=await userModel.findOne({"eMail":eMail})
+    if(!data)
+    {
+        return res.json(
+            {
+                status:"invalid user"
+            }
+        )
+    }
+    console.log(data)
+    let dbPassword=data.password
+    let inputPassword=req.body.password
+    console.log(dbPassword)
+    console.log(inputPassword)
+    const match=await bcrypt.compare(inputPassword,dbPassword)
+    if(!match)
+    {
+        return res.json(
+            {
+                status:"invalid password"
+            }
+        )
+    }
+
+    res.json({
+        status:"success"
+    })
+
  })
 
  module.exports=router
